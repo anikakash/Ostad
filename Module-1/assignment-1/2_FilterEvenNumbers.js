b4435758f41cf5737e1cfd049b15328790aae55c
anikakash <anik15-14210@diu.edu.bd>
2023-07-28 14:51:53 +0600

    update even odd using array filter

--- Module-1/assignment-1/2_FilterEvenNumbers.js
+++ Module-1/assignment-1/2_FilterEvenNumbers.js
@@ -1,6 +1,6 @@
  let nums = [12, 34, 45, 23, 6, 78, 54, 90];
- for(let i=0; i<nums.length; i++){
-    if(nums[i]%2==0){
-        console.log(nums[i]);
-    }
- }
+
+ let evenNums = nums.filter(function (num) {
+        return num % 2 == 0;
+    });
+    console.log(evenNums);
