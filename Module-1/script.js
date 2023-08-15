c8832f5183ad960c2c9f947773da626ee9a794e7
anikakash <anik15-14210@diu.edu.bd>
2023-08-15 23:32:31 +0600

    new things

---
+++ Module-1/script.js
@@ -0,0 +1,5 @@
+// Array Filter
+
+let num = [30,20,40,55,60, 80, 90];
+
+num.filter(function (){})
--- Object.js
+++ Object.js
@@ -15,18 +15,28 @@
 
 
 /// This can use in a function also:
-function sayHello() {
-    console.log("Hello, " + this.name + "!");
-}
-
-const person = {
-    name: "Alice",
-    age: 25
-};
-
-sayHello.call(person);
+// function sayHello() {
+//     console.log("Hello, " + this.name + "!");
+// }
+//
+// const person = {
+//     name: "Alice",
+//     age: 25
+// };
+//
+// sayHello.call(person);
 
 
 
 // -------------- End this use in function ------------
 
+console.log("Hello 1");
+
+setInterval(function(){
+    console.log("Hello SetTimeOut");
+}, 1000);
+
+console.log("Hello 2");
+console.log("Hello 3");
+
+console.log("This is anik");
