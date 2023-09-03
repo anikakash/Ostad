e22d10393c5c778633d9f1fc807d702274d994ce
anikakash <anik15-14210@diu.edu.bd>
2023-09-03 23:43:49 +0600

    module -6

---
+++ JS/primitives_object.js
@@ -0,0 +1,26 @@
+//NN BB SS U
+// NULL
+// NUMBER
+// SYMBOL
+// STRING
+// BOOLEAN
+// BIGINT
+// UNDEFINED
+
+let a = null;
+let b = 234;
+let c = true;
+let d = BigInt("567") + BigInt("3");
+let e = "Harry";
+let f = Symbol("I am a nice symbol");
+let g
+console.log(a, b, c, d, e, f, g);
+console.log(typeof c);
+
+// Object in js
+
+const item = {
+    "Harry" : true,
+    "Shub" : false,
+    "Lovish" : 67
+}
--- Module-5/JsPrac/new.js
+++ Module-5/JsPrac/new.js
@@ -1,1 +1,10 @@
-console.log("Hello")
+const http = require('http');
+
+const requestHandler = (req, res) => {
+    res.setHeader('Content-Type', 'text/plain');
+    res.write('Hello This is Node Js Server!');
+    res.end();
+};
+const server = http.createServer(requestHandler);
+server.listen(5500, () => {
+});
