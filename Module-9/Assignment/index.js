d08038b62c7a4950e291e090f69e4874f65a60dd
Anik Dash Akash <anik15-14210@diu.edu.bd>
2023-10-31 04:47:20 +0600

    Delete Module-9/Assignment directory

--- Module-9/Assignment/index.js
+++
@@ -1,7 +0,0 @@
-const app = require("./app"); // Import your Express app from app.js
-
-const PORT = process.env.RUNNING_PORT || 8080; // Use the PORT from config.env, or default to 8080
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
